5f77c3ec7ed7daa78b81b6623183ea5f57c34ab5
Johannes Bauer <joe@johannes-bauer.com>
2020-09-25 22:33:42 +0200

    Massive refactoring of UI handling
    Making the UI state publicly accessibly by ISRs so we can get triggers
    whenever audio IRQs occur.

--- main.c
+++ main.c
@@ -59,8 +59,15 @@ enum turn_signal_state_t {
 	TURN_EMERGENCY,
 };
 
+enum engine_state_t {
+	ENGINE_OFF,
+	ENGINE_CRANKING,
+	ENGINE_ON,
+	ENGINE_SHUTTING_OFF,
+};
+
 struct uistate_t {
-	bool engine_running;
+	enum engine_state_t engine_state;
 	bool siren;
 	enum turn_signal_state_t turn_signal;
 	unsigned int undervoltage_counter;
@@ -74,7 +81,19 @@ struct uistate_t {
 	unsigned int siren_blink_counter;
 };
 
-volatile unsigned int timectr = 0;
+static volatile unsigned int timectr = 0;
+static const struct debounce_config_t default_button_config = {
+	.fire_threshold = 10,
+};
+static struct uistate_t ui = {
+	.button_left.config = &default_button_config,
+	.button_right.config = &default_button_config,
+	.button_siren.config = &default_button_config,
+	.button_parent.config = &default_button_config,
+	.button_ignition_crank.config = &default_button_config,
+	.button_ignition_ccw.config = &default_button_config,
+	.ignition_state.config = &default_button_config,
+};
 
 static void wait_systick(void) {
 	unsigned int old_timectr = timectr;
@@ -98,49 +117,56 @@ static void enter_error_mode(unsigned int error_code) {
 	}
 }
 
-static enum ignition_state_t determine_ignition_state(const struct uistate_t *state) {
+static enum ignition_state_t determine_ignition_state(void) {
 	enum ignition_state_t ignition_state = IGNITION_UNDEFINED;
 	bool have_power = pwr_sense_is_active();
 //	printf("Crank %d CCW %d Pwr %d\n", state.button_ignition_crank.last_state, state.button_ignition_ccw.last_state, pwr_sense_is_active());
 	if (have_power) {
 		/* Either "ignition on" or "ignition crank" */
-		if ((state->button_ignition_crank.last_state == 0) && (state->button_ignition_ccw.last_state == 1)) {
+		if ((ui.button_ignition_crank.last_state == 0) && (ui.button_ignition_ccw.last_state == 1)) {
 			ignition_state = IGNITION_ON;
-		} else if ((state->button_ignition_crank.last_state == 1) && (state->button_ignition_ccw.last_state == 0)) {
+		} else if ((ui.button_ignition_crank.last_state == 1) && (ui.button_ignition_ccw.last_state == 0)) {
 			ignition_state = IGNITION_CRANK;
 		}
 	} else {
 		/* Either "ignition off" or "ignition CCW" */
-		if ((state->button_ignition_crank.last_state == 0) && (state->button_ignition_ccw.last_state == 0)) {
+		if ((ui.button_ignition_crank.last_state == 0) && (ui.button_ignition_ccw.last_state == 0)) {
 			ignition_state = IGNITION_OFF;
-		} else if ((state->button_ignition_crank.last_state == 0) && (state->button_ignition_ccw.last_state == 1)) {
+		} else if ((ui.button_ignition_crank.last_state == 0) && (ui.button_ignition_ccw.last_state == 1)) {
 			ignition_state = IGNITION_CCW;
 		}
 	}
 	return ignition_state;
 }
 
-static void audio_change(const struct uistate_t *state) {
-	if (state->engine_running) {
-		if (state->siren) {
+static void audio_update(void) {
+	if (ui.engine_state == ENGINE_CRANKING) {
+		audio_playback_fileno(FILENO_ENGINE_START, true);
+	} else if (ui.engine_state == ENGINE_SHUTTING_OFF) {
+		audio_playback_fileno(FILENO_ENGINE_STOP, true);
+	} else if (ui.engine_state == ENGINE_ON) {
+		if (ui.siren) {
 			audio_playback_fileno(FILENO_SIREN_WITH_ENGINE, true);
 		} else {
 			audio_playback_fileno(FILENO_ENGINE_IDLE, true);
 		}
-	} else {
-		if (state->siren) {
+	} else if (ui.engine_state == ENGINE_OFF) {
+		if (ui.siren) {
 			audio_playback_fileno(FILENO_SIREN_NO_ENGINE, true);
 		} else {
-			audio_playback_fileno(FILENO_ENGINE_STOP, true);
+			audio_shutoff();
 		}
 	}
 }
 
 void audio_trigger_end_of_sample(unsigned int fileno) {
 	if (fileno == FILENO_ENGINE_START) {
-//		audio_change(state);
+		/* Start of engine is finished */
+		ui.engine_state = ENGINE_ON;
+		audio_update();
 	} else if (fileno == FILENO_ENGINE_STOP) {
-		audio_shutoff();
+		ui.engine_state = ENGINE_OFF;
+		audio_update();
 	}
 }
 
@@ -154,77 +180,61 @@ int main(void) {
 	while (timectr < 125);		/* Wait 250 ms before flash settles */
 	audio_init();
 
-	const struct debounce_config_t default_button_config = {
-		.fire_threshold = 10,
-	};
-	struct uistate_t state = {
-		.button_left.config = &default_button_config,
-		.button_right.config = &default_button_config,
-		.button_siren.config = &default_button_config,
-		.button_parent.config = &default_button_config,
-		.button_ignition_crank.config = &default_button_config,
-		.button_ignition_ccw.config = &default_button_config,
-		.ignition_state.config = &default_button_config,
-	};
 	while (true) {
 		/* Execute roughly 100 Hz */
 		wait_systick();
 
 		uint32_t voltage_millivolts = adc_get_ext_voltage_millivolts();
 		if (voltage_millivolts < 3500 * 3) {
-			state.undervoltage_counter++;
+			ui.undervoltage_counter++;
 		} else {
-			state.undervoltage_counter = 0;
+			ui.undervoltage_counter = 0;
 		}
-		if (state.undervoltage_counter >= 100) {
+		if (ui.undervoltage_counter >= 100) {
 			/* One second of straight undervoltage, go into error mode. */
 			enter_error_mode(0);
 		}
 
-		if (debounce_button_active(&state.button_left, button_left_is_active())) {
+		if (debounce_button_active(&ui.button_left, button_left_is_active())) {
 			printf("Left\n");
 		}
-		if (debounce_button_active(&state.button_right, button_right_is_active())) {
+		if (debounce_button_active(&ui.button_right, button_right_is_active())) {
 			printf("Right\n");
 		}
-		if (debounce_button_active(&state.button_parent, button_parent_is_active())) {
+		if (debounce_button_active(&ui.button_parent, button_parent_is_active())) {
 			printf("Parent\n");
 		}
-		if (debounce_button_active(&state.button_siren, button_siren_is_active())) {
-			state.siren = !state.siren;
-			if (state.siren) {
+		if (debounce_button_active(&ui.button_siren, button_siren_is_active())) {
+			ui.siren = !ui.siren;
+			if (ui.siren) {
 				led_siren_set_active();
 			} else {
-				state.siren_blink_counter = 0;
+				ui.siren_blink_counter = 0;
 			}
-			audio_change(&state);
+			audio_update();
 		}
 
-		debounce_button(&state.button_ignition_crank, ignition_crank_is_active());
-		debounce_button(&state.button_ignition_ccw, ignition_ccw_is_active());
-		enum ignition_state_t current_ignition_state = determine_ignition_state(&state);
-		bool ignition_state_changed = debounce_button(&state.ignition_state, current_ignition_state);
+		debounce_button(&ui.button_ignition_crank, ignition_crank_is_active());
+		debounce_button(&ui.button_ignition_ccw, ignition_ccw_is_active());
+		enum ignition_state_t current_ignition_state = determine_ignition_state();
+		bool ignition_state_changed = debounce_button(&ui.ignition_state, current_ignition_state);
 		if (ignition_state_changed) {
-			bool change_audio = false;
-			if ((!state.engine_running) && (state.ignition_state.last_state == IGNITION_CRANK)) {
-				state.engine_running = true;
-				change_audio = true;
-			} else if (state.engine_running && ((state.ignition_state.last_state == IGNITION_OFF) || (state.ignition_state.last_state == IGNITION_CCW))) {
-				state.engine_running = false;
-				change_audio = true;
-			}
-			if (change_audio) {
-				audio_change(&state);
+			if ((ui.engine_state == ENGINE_OFF) && (ui.ignition_state.last_state == IGNITION_CRANK)) {
+				ui.engine_state = ENGINE_CRANKING;
+				audio_update();
+			} else if (((ui.engine_state == ENGINE_ON) || (ui.engine_state == ENGINE_CRANKING)) && ((ui.ignition_state.last_state == IGNITION_OFF) || (ui.ignition_state.last_state == IGNITION_CCW))) {
+				ui.engine_state = ENGINE_SHUTTING_OFF;
+				audio_update();
 			}
 		}
 
-		if (!state.siren) {
+		if (!ui.siren) {
 			led_siren_set_inactive();
 		} else {
-			state.siren_blink_counter++;
-			if (state.siren_blink_counter > 30) {
+			ui.siren_blink_counter++;
+			if (ui.siren_blink_counter > 30) {
 				led_siren_toggle();
-				state.siren_blink_counter = 0;
+				ui.siren_blink_counter = 0;
 			}
 		}
 
